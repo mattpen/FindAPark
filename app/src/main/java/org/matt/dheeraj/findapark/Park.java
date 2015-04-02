f7fbf9d73b7ff4eeaeddbbedc8335649390a5d1b
mattpen <matthew.pennington@colorado.edu>
2015-04-02 08:48:57 -0600

    Initial commit

---
+++ app/src/main/java/org/matt/dheeraj/findapark/Park.java
@@ -0,0 +1,71 @@
+package org.matt.dheeraj.findapark;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
+/**
+ * Created by Matt on 3/31/2015.
+ */
+public class Park implements Parcelable{
+    //Public Methods
+    public String getName() {return name;}
+    public int getRating() {
+        if (ratingCounter > 0)
+            return (ratingSum / ratingCounter);
+
+        else
+            return 0;
+    }
+    public void addRating(int newRating) {
+        if ((newRating > 0) && (newRating < 6)){
+            ratingCounter++;
+            ratingSum += newRating;
+        }
+        else
+            throw new IllegalArgumentException();
+    }
+    public void writeToParcel(Parcel dest, int flags){
+        dest.writeString(name);
+        dest.writeInt(ratingSum);
+        dest.writeInt(ratingCounter);
+    }
+    private void readFromParcel(Parcel in){
+        name = in.readString();
+        ratingSum = in.readInt();
+        ratingCounter = in.readInt();
+    }
+    public int describeContents(){
+        return CONTENTS_FILE_DESCRIPTOR ;
+    }
+    //Constructor(s)
+    public Park (String n){
+        name = n;
+    }
+
+    public Park (){throw new UnsupportedOperationException();}
+
+    public Park(Parcel in){
+        name = in.readString();
+        ratingSum = in.readInt();
+        ratingCounter = in.readInt();
+    }
+    //Private Members
+    private String name;
+    private int ratingSum;
+    private int ratingCounter;
+    //List<ParkFeature> features;
+    //List<ParkComment> comments;
+
+    //Private Methods
+    public static final Parcelable.Creator<Park> CREATOR = new Parcelable.Creator<Park>() {
+        public Park createFromParcel(Parcel in) {
+            return new Park(in);
+        }
+
+        public Park[] newArray(int size) {
+            return new Park[size];
+        }
+    };
+}
--- app/src/main/java/org/matt/dheeraj/findapark/SaveParkActivity.java
+++ app/src/main/java/org/matt/dheeraj/findapark/SaveParkActivity.java
@@ -1,31 +1,38 @@
 package org.matt.dheeraj.findapark;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.Editable;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import java.util.IllformedLocaleException;
 
-
-public class AddAParkActivity extends ActionBarActivity {
+public class SaveParkActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_save_park);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        Park park = bundle.getParcelable("newpark");
 
-        setContentView(R.layout.activity_add_apark);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(park.getName());
+        setContentView(textView);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_apark, menu);
+        getMenuInflater().inflate(R.menu.menu_save_park, menu);
         return true;
     }
 
@@ -44,12 +51,17 @@ public class AddAParkActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void savePark(View view){
+    public static class PlaceholderFragment extends Fragment {
 
-        Intent intent = new Intent(this, SaveParkActivity.class);
-        Park park = new Park(((EditText) findViewById(R.id.name)).getText().toString());
-        //Other Park stuff
-        intent.putExtra("newpark",park);
-        startActivity(intent);
+        public PlaceholderFragment() { }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_save_park,
+                    container, false);
+            return rootView;
+        }
     }
+
 }
